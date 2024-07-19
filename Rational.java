c45388a525013f8cf8c75a81dae0bf0f9f6e5ca8
이은지 <78470571+siddltkfkd@users.noreply.github.com>
2024-07-19 13:47:57 +0900

    Refactor: int 변수와 연산 추가

--- Rational.java
+++ Rational.java
@@ -22,6 +22,10 @@ public class Rational {
     public Rational add(Rational other) {
         return new Rational(this.getNominator() * other.getDenominator() + this.getDenominator() * other.getNominator(), this.getDenominator() * other.getDenominator());
     }
+
+    public Rational add(int i) {
+        return new Rational(i * this.getDenominator() + this.getNominator(), this.getDenominator());
+    }
     
     public Rational substract(Rational r1, Rational r2) {
         return new Rational(r1.getNominator() * r2.getDenominator() - r1.getDenominator() * r2.getNominator(), r1.getDenominator() * r2.getDenominator());
@@ -29,6 +33,10 @@ public class Rational {
     
     public Rational substract(Rational other) {
         return new Rational(this.getNominator() * other.getDenominator() - this.getDenominator() * other.getNominator(), this.getDenominator() * other.getDenominator());
+    }
+
+    public Rational substract(int i) {
+        return new Rational(this.nominator - this.getDenominator() * i, this.getDenominator());
     }
 
     public Rational multiply(Rational r1, Rational r2) {
@@ -39,12 +47,20 @@ public class Rational {
         return new Rational(this.getNominator() * other.getNominator(), this.getDenominator() * other.getDenominator());
     }
 
+    public Rational multiply(int i) {
+        return new Rational(this.getNominator() * i, this.getDenominator());
+    }
+
     public Rational divide(Rational r1, Rational r2) {
         return new Rational(r1.getNominator() * r2.getDenominator(), r1.getDenominator() * r2.getNominator());
     }
     
     public Rational divide(Rational other) {
         return new Rational(this.getNominator() * other.getDenominator(), this.getDenominator() * other.getNominator());
+    }
+
+    public Rational divide(int i) {
+        return new Rational(this.getNominator(), this.getDenominator() * i);
     }
 
     private int gcd(int a, int b) {
@@ -60,6 +76,15 @@ public class Rational {
         System.out.printf("%s / %s = %s\n",r1, r2, r1.divide(r2));
     }
 
+    public static void test2() {
+        Rational r1 = new Rational(5, 8);
+        int i = 5;
+        System.out.printf("%s + %d = %s\n", r1, i, r1.add(i));
+        System.out.printf("%s - %d = %s\n", r1, i, r1.substract(i));
+        System.out.printf("%s * %d = %s\n",r1, i, r1.multiply(i));
+        System.out.printf("%s / %d = %s\n",r1, i, r1.divide(i));
+    }
+
     @Override
     public String toString() {
         return this.getNominator() + "/" + this.getDenominator();
@@ -68,6 +93,6 @@ public class Rational {
 
 class Test {
     public static void main(String[] args) {
-        Rational.test();
+        Rational.test2();
     }
 }
